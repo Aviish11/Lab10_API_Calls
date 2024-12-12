e102568f8451b73eeca177cc87fbcf6ad4958a52
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-12-12 01:13:28 -0500

    Task 2: API Interaction Using XMLHttpRequest

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const fetchData = document.getElementById("fetch");
 const xhr = document.getElementById("xhr");
 
+fetchData.addEventListener('click', function() {
 fetch("https://jsonplaceholder.typicode.com/posts/1")
         .then(response => {
             if (!response.ok) {
@@ -16,4 +17,28 @@ fetch("https://jsonplaceholder.typicode.com/posts/1")
         .catch(error => {
             displayData.textContent = `Error: ${error.message}`;
         });
+    });
+
+
+    xhr.addEventListener("click", function() {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/2");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) { 
+            if (xhr.status === 200) { 
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+                displayData.innerHTML = ('data.inputTitle');
+                displayData.innerHTML = ('data.inputBody');
+            } else {
+                console.error('Error fetching data:', xhr.statusText);
+            }
+        }
+    };
+
+    xhr.send();
+});
+
 
